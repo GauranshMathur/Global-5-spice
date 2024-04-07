e8522489509f3e64363a53c1845f9c282c397013
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2024-04-07 10:12:02 +0800

    Updated json convert

--- Main.py
+++ Main.py
@@ -2,24 +2,20 @@ import os
 from API_Key import key  # Import the API key from your module
 from openai import OpenAI
 
-def fine_tune_model(training_file_path, model_name="gpt-3.5-turbo"):
-    # Set the OPENAI_API_KEY environment variable
-    os.environ['OPENAI_API_KEY'] = key  # Set the API key
+# Set the OPENAI_API_KEY environment variable
+os.environ['OPENAI_API_KEY'] = key  # Set the API key
 
-    # Create an instance of the OpenAI client
-    client = OpenAI()
+# Create an instance of the OpenAI client
+client = OpenAI()
 
-    file_id = ""
+# Specify the path to the training file
+training_file_path = "data/data.jsonl"
 
-    # Create a fine-tuning job
-    client.fine_tuning.jobs.create(
-        training_file=file_id,
-        model=model_name  # Specify the model name
-    )
+# Specify the model name
+model_name = "gpt-3.5-turbo"  # Using the GPT-3.5 Turbo model
 
-if __name__ == "__main__":
-    # Specify the path to the training file
-    training_file_path = "data/test.json"
-
-    # Call the function to initiate fine-tuning
-    fine_tune_model(training_file_path)
+# Create a fine-tuning job
+client.fine_tuning.jobs.create(
+    training_file=training_file_path,
+    model=model_name
+)
--- json_convert.py
+++ json_convert.py
@@ -1,29 +1,51 @@
 import json
+import os
 
-input_file = "data/ipos_data_page_1.json"
-output_file = "data/data.json"
+input_file = "merged_file.json"
+output_file = "data/data.jsonl"
+
+
+Count = 0 
 
 with open(input_file, 'r') as f:
     data = json.load(f)
+    
+if os.path.exists(output_file):
+# Delete the file
+    os.remove(output_file)
+    
+with open(output_file, 'a') as f:
+    
 
-for item in data['questionListItems']:
-    user = item['title']
-    assistant = item['answer']['body']
-    assistant = assistant.replace("<p>", "")
-    assistant = assistant.replace("</p>", "")
-
-    conversation = {
-        "messages": [
-            {"role": "system", "content": "You are a goverment chatbot that needs to help businesses with finding services"},
-            {"role": "user", "content": user},
-            {"role": "assistant", "content": assistant}
-        ]
-    }
-
-    with open(output_file, 'a') as f:
+    for item in data['questionListItems']:
+        user = item['title']
+        assistant = repr(item['answer']['body'])
+        assistant = assistant.replace("<p>", "")
+        assistant = assistant.replace("</p>", "")
+        
+        #print(assistant)
+        
+        if "<" in assistant or "\\" in assistant:
+            continue
+        
+        #print(assistant)
+        
+        conversation = {
+            "messages": [
+                {"role": "system", "content": "You are a goverment chatbot that needs to help businesses with finding services"},
+                {"role": "user", "content": user},
+                {"role": "assistant", "content": assistant}
+            ]
+        }
+        
+        if Count == 5:
+            break
+        else:    
+            Count = Count + 1
+        
         json.dump(conversation, f)
         f.write('\n')  # Add a newline character to separate conversations
-    
+
 
   
 print("Conversion completed. Output saved to:", output_file)
